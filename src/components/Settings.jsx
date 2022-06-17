8365642709980ca61308fc8e83fde2aa5365489a
RoxZkiL <mttrox@gmail.com>
2022-06-17 13:53:19 -0400

    Initial commit

---
+++ src/components/Settings.jsx
@@ -0,0 +1,37 @@
+import ReactSlider from "react-slider";
+import { useContext } from "react";
+import SettingsContext from "./SettingsContext";
+import "./slider.css";
+import BackButton from "./BackButton";
+function Settings() {
+  const settingsInfo = useContext(SettingsContext);
+  return (
+    <div style={{ textAlign: "left" }}>
+      <label>work: {settingsInfo.workMinutes}:00</label>
+      <ReactSlider
+        className={"slider"}
+        thumClassName={"thumb"}
+        trackClassName={"track"}
+        value={settingsInfo.workMinutes}
+        onChange={(newValue) => settingsInfo.setWorkMinutes(newValue)}
+        min={1}
+        max={120}
+      />
+      <label>break: {settingsInfo.breakMinutes}:00</label>
+      <ReactSlider
+        className={"slider green"}
+        thumClassName={"thumb"}
+        trackClassName={"track"}
+        value={settingsInfo.breakMinutes}
+        onChange={(newValue) => settingsInfo.setBreakMinutes(newValue)}
+        min={1}
+        max={120}
+      />
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <BackButton onClick={() => settingsInfo.setShowSettings(false)} />
+      </div>
+    </div>
+  );
+}
+
+export default Settings;
